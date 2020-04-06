a18328d4b682e06bcf5b8a387e56f8941598d05a
Justin Murillo <boxmonkey13@outlook.com>
2020-04-06 03:52:36 -0600

    Team_Stats and Server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const dbConfig = {
 	port: 5432,
 	database: 'football_db',
 	user: 'postgres',
-	password: 'Galaga99'
+	password: 'ImissSazy99!'
 };
 
 var db = pgp(dbConfig);
@@ -43,29 +43,24 @@ app.use(express.static(__dirname + '/'));//This line is necessary for us to use 
    - Database access
    - Parse parameters from get (URL) and post (data package)
    - Render Views - This will decide where the user will go after the get/post request has been processed
-
  Web Page Requests:
-
   Login Page:        Provided For your (can ignore this page)
   Registration Page: Provided For your (can ignore this page)
   Home Page:
   		/home - get request (no parameters)
   				This route will make a single query to the favorite_colors table to retrieve all of the rows of colors
   				This data will be passed to the home view (pages/home)
-
   		/home/pick_color - post request (color_message)
   				This route will be used for reading in a post request from the user which provides the color message for the default color.
   				We'll be "hard-coding" this to only work with the Default Color Button, which will pass in a color of #FFFFFF (white).
   				The parameter, color_message, will tell us what message to display for our default color selection.
   				This route will then render the home page's view (pages/home)
-
   		/home/pick_color - get request (color)
   				This route will read in a get request which provides the color (in hex) that the user has selected from the home page.
   				Next, it will need to handle multiple postgres queries which will:
   					1. Retrieve all of the color options from the favorite_colors table (same as /home)
   					2. Retrieve the specific color message for the chosen color
   				The results for these combined queries will then be passed to the home view (pages/home)
-
   		/team_stats - get request (no parameters)
   			This route will require no parameters.  It will require 3 postgres queries which will:
   				1. Retrieve all of the football games in the Fall 2018 Season
@@ -74,7 +69,6 @@ app.use(express.static(__dirname + '/'));//This line is necessary for us to use 
   			The three query results will then be passed onto the team_stats view (pages/team_stats).
   			The team_stats view will display all fo the football games for the season, show who won each game,
   			and show the total number of wins/losses for the season.
-
   		/player_info - get request (no parameters)
   			This route will handle a single query to the football_players table which will retrieve the id & name for all of the football players.
   			Next it will pass this result to the player_info view (pages/player_info), which will use the ids & names to populate the select tag for a form
@@ -111,13 +105,44 @@ app.get('/home', function(req, res) {
         .catch(function (err) {
             // display error message in case an error
             console.log('error', err);
-            res.render('pages/home', {
+            response.render('pages/home', {
                 title: 'Home Page',
                 data: '',
                 color: '',
                 color_msg: ''
             })
         })
+});
+
+app.get('/home/pick_color', function(req, res) {
+	var color_choice = req.query.color_selection;
+	var color_options =  'select * from favorite_colors;';
+	var color_message = "select color_msg from favorite_colors where hex_value = '" + color_choice + "';";
+	db.task('get-everything', task => {
+        return task.batch([
+            task.any(color_options),
+            task.any(color_message)
+        ]);
+    })
+    .then(info => {
+    	res.render('pages/home',{
+				my_title: "Home Page",
+				data: info[0],
+				color: color_choice,
+				color_msg: info[1][0].color_msg
+			})
+    })
+    .catch(err => {
+        // display error message in case an error
+            console.log('error', err);
+            response.render('pages/home', {
+                title: 'Home Page',
+                data: '',
+                color: '',
+                color_msg: ''
+            })
+    });
+
 });
 
 app.post('/home/pick_color', function(req, res) {
@@ -145,7 +170,7 @@ app.post('/home/pick_color', function(req, res) {
     .catch(err => {
         // display error message in case an error
             console.log('error', err);
-            res.render('pages/home', {
+            response.render('pages/home', {
                 title: 'Home Page',
                 data: '',
                 color: '',
@@ -153,5 +178,92 @@ app.post('/home/pick_color', function(req, res) {
             })
     });
 });
+
+app.get('/team_stats', function(req, res) {
+	var query1 = 'SELECT * FROM football_games;';
+	var query2 = 'SELECT COUNT(*) FROM football_games WHERE home_score > visitor_score;';
+	var query3 = 'SELECT COUNT(*) FROM football_games WHERE home_score < visitor_score;';
+	db.task('get-everything', task => {
+    return task.batch([
+        task.any(query1),
+        task.any(query2),
+        task.any(query3)
+        ]);
+    })
+    .then(data => {
+    	res.render('pages/team_stats',{
+    			my_title: "Team Stats",
+    			result_1: data[0],
+    			result_2: data[1],
+    			result_3: data[2],
+                win: data[1][0],
+                loss: data[2][0]
+    		})
+    })
+    .catch(err => {
+        // display error message in case an error
+            console.log('error', err);
+            res.render('pages/team_stats',{
+    			my_title: "Team Stats",
+    			result_1: '',
+    			result_2: '',
+    			result_3: '',
+    		})
+    });
+});
+
+app.get('/player_info', function(req, res) {
+    var query = "SELECT * FORM football_players;";
+    db.task('get-everything', task => {
+        return task.batch([
+            task.any(query)
+        ]);
+    })
+    .then(data => {
+  	     res.render('pages/player_info',{
+  			my_title: "Player Info",
+            players_table: data[0],
+            player_info: '',
+            result: ''
+  		})
+    })
+    .catch(err => {
+        console.log('error', err);
+        res.render('pages/team_stats',{
+  			my_title: "Player Info",
+            players_table: '',
+            dropdown_player: '',
+            result: ''
+  		})
+    });
+});
+
+app.get('/player_info/post', function(req, res) {
+    var player_id = req.query.player_choice;
+    var query = 'SELECT * FROM football_players WHERE id=' + player_id + ';';
+    var query2 = 'SELECT COUNT(players) FORM football_games WHERE ' + player_id + '=ANY(players);';
+    db.task('get-everything', task => {
+        return task.batch([
+            task.any(query),
+            task.any(query2)
+        ]);
+    })
+    .then(data => {
+    	res.render('pages/player_info',{
+			my_title: "Player Info",
+            players_table: data[0][0],
+            result: data[1][0].count
+		})
+    })
+    .catch(err => {
+        console.log('error', err);
+        res.render('pages/player_info',{
+			my_title: "Player Info",
+            players_table: '',
+            result: ''
+		})
+    });
+});
+
 app.listen(3000);
 console.log('3000 is the magic port');
